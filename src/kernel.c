b376bbc7159b5da0f5588d7732cfbb3c3aee383d
foxostro <foxostro@gmail.com>
2016-12-09 11:22:05 -0800

    Added some comments

--- src/kernel.c
+++ src/kernel.c
@@ -27,21 +27,34 @@ void keyboard_int_handler()
 
 void kernel_main(void *mb_info, void *istack)
 {
+    // Setup the initial Task State Segment. The kernel uses one TSS between all
+    // tasks and performs software task switching.
     bzero(&s_tss, sizeof(s_tss));
     s_tss.ss0 = SEGSEL_KERNEL_DS;
     s_tss.esp0 = (uint32_t)istack;
     s_tss.iomap = sizeof(s_tss);
 
+    // Setup the Global Descriptor Table. The kernel uses a flat memory map
+    // and so the GDT only requires five entries:
+    // 1 -- descriptor for the TSS
+    // 2 -- descriptor for kernel code
+    // 3 -- descriptor for kernel data
+    // 4 -- descriptor for user space code
+    // 5 -- descriptor for user space data
     bzero(s_gdt, sizeof(s_gdt));
     gdt_create_flat_mapping(s_gdt, sizeof(s_gdt), (uint32_t)&s_tss);
     lgdt(s_gdt, sizeof(s_gdt) - 1);
     load_task_register();
 
+    // Setup the Interrupt Descriptor Table. This wires various IRQs up to their
+    // handler functions.
     bzero(s_idt, sizeof(idt_entry_t) * IDT_ENTS);
     idt_build_entry(&s_idt[KEY_IDT_ENTRY], (uint32_t)asm_keyboard_wrapper, INTERRUPT_GATE, 0);
     idt_build_entry(&s_idt[TIMER_IDT_ENTRY], (uint32_t)asm_timer_wrapper, INTERRUPT_GATE, 0);
     lidt(s_idt, sizeof(s_idt) - 1);
 
+    // Initialize the PIC chip. It is necessary to remap IRQs before enabling
+    // interrupts.
     pic_init();
 
     console_init((vgachar_t *)0xB8000);
@@ -49,9 +62,13 @@ void kernel_main(void *mb_info, void *istack)
     kprintf("mb_info = %p\n", mb_info);
     kprintf("istack = %p\n", istack);
 
+    // Configure the PIC timer chip so that it fires an interrupt every 10ms.
     timer_init(TIMER_RATE_10ms, TIMER_LEAP_INTERVAL_10ms, TIMER_LEAP_TICKS_10ms);
 
+    // After this point, interrupts will start firing.
     enable_interrupts();
 
+    // Spin forever. After this point, the kernel is entirely driven in response
+    // to interrupts.
     for(;;);
 }
