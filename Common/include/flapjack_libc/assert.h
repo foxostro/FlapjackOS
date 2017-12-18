2f6b0540b6a6a6d695bc9681359e4c1b45b66c11
foxostro <foxostro@gmail.com>
2017-12-17 17:59:37 -0800

    Do not include stdlib.h in assert.h if building for cross-compile.

--- Common/include/flapjack_libc/assert.h
+++ Common/include/flapjack_libc/assert.h
@@ -1,12 +1,15 @@
 #pragma once
 
+#ifdef TESTING
+
 #include <stdlib.h>
+#define assert(expr) ((void)((expr) ? 0 : (abort(), 0)))
+
+#else // TESTING
 
 // Panic is defined in libKernel. Need to figure out how to resolve this circular dependency.
 __attribute__((noreturn)) void panic(const char * restrict fmt, ...);
 
-#ifdef TESTING
-#define assert(expr) ((void)((expr) ? 0 : (abort(), 0)))
-#else
 #define assert(expr) ((void)((expr) ? 0 : (panic("%s:%u: failed assertion `%s'", __FILE__, __LINE__, # expr), 0)))
-#endif
+
+#endif // TESTING
