704423cd7cc101c0c32713ebfcafef0b62444c8e
foxostro <foxostro@gmail.com>
2017-12-30 22:13:41 -0800

    ring_buffer is a deque with access at both the front and the back.

--- Common/include/common/ring_buffer.hpp
+++ Common/include/common/ring_buffer.hpp
@@ -2,8 +2,9 @@
 
 #include <cstddef>
 #include <cstdint>
+#include <cstring> // for memset
 #include <cassert>
-#include <utility> // for std::move()
+#include <utility> // for std::move
 #include <type_traits> // for std::aligned_storage
 #include <new> // for placement-new
 
@@ -12,14 +13,94 @@
 template<typename TYPE, int CAPACITY>
 class ring_buffer {
     using value_type = TYPE;
-    using size_type = int;
+    using size_type = size_t;
 
     static_assert(CAPACITY > 0, "CAPACITY must be greater than zero.");
     using internal_type = typename std::aligned_storage<sizeof(value_type), alignof(value_type)>::type;
 
+    // An index into the buffer which wraps around at CAPACITY.
+    // This can be incremented, decremented, and cast to size_type.
+    class circular_index {
+        size_type _value;
+
+    public:
+        circular_index(size_type value) : _value(value) {}
+        circular_index(const circular_index &other) : _value(other._value) {}
+
+        operator size_type() const
+        {
+            return _value;
+        }
+
+        bool operator==(const circular_index &other) const
+        {
+            if (this == &other) {
+                return true;
+            }
+
+            return (other._value == _value);
+        }
+
+        bool operator!=(const circular_index &other) const
+        {
+            return !(*this == other);
+        }
+
+        circular_index& operator=(const circular_index &other)
+        {
+            if (this != &other) {
+                _value = other._value;
+            }
+
+            return *this;
+        }
+
+        circular_index& operator++()
+        {
+            _value = (_value + 1) % CAPACITY;
+            return *this;
+        }
+
+        circular_index operator++(int)
+        {
+            _value = (_value + 1) % CAPACITY;
+            return *this;
+        }
+
+        circular_index& operator--()
+        {
+            if (_value == 0) {
+                _value = CAPACITY - 1;
+            } else {
+                _value = (_value - 1) % CAPACITY;
+            }
+            return *this;
+        }
+
+        circular_index operator--(int)
+        {
+            if (_value == 0) {
+                _value = CAPACITY - 1;
+            } else {
+                _value = (_value - 1) % CAPACITY;
+            }
+            return *this;
+        }
+    };
+
+    circular_index index_at(size_type index)
+    {
+        assert(index >= 0 && index < _count);
+        circular_index cursor(_front_pos);
+        for (size_type i = 0; i < index; ++i) {
+            ++cursor;
+        }
+        return cursor;
+    }
+
     internal_type _buffer[CAPACITY];
-    size_type _dequeue_pos;
-    size_type _enqueue_pos;
+    circular_index _front_pos;
+    circular_index _back_pos;
     size_type _count;
     
 public:
@@ -30,46 +111,132 @@ public:
         }
     }
 
-    ring_buffer() : _dequeue_pos(0), _enqueue_pos(0), _count(0) {}
+    ring_buffer() : _front_pos(0), _back_pos(0), _count(0) {}
 
-    // Enqueues the specified value into the ring_buffer.
+    // Returns the element at the front of the buffer.
+    value_type& front()
+    {
+        assert(!empty());
+        return *((value_type *)_buffer + _front_pos);
+    }
+
+    // Returns the element at the front of the buffer.
+    const value_type& front() const
+    {
+        assert(!empty());
+        return *((value_type *)_buffer + _front_pos);
+    }
+
+    // Returns the element at the back of the buffer.
+    value_type& back()
+    {
+        assert(!empty());
+        return *((value_type *)_buffer + _back_pos);
+    }
+
+    // Returns the element at the back of the buffer.
+    const value_type& back() const
+    {
+        assert(!empty());
+        return *((value_type *)_buffer + _back_pos);
+    }
+
+    // Returns the i'th element of the ring_buffer.
+    // The elements are indexed with zero being the front cursor.
+    value_type& at(size_type index)
+    {
+        circular_index cursor = index_at(index);
+        return *((value_type *)_buffer + cursor);
+    }
+
+    // Returns the i'th element of the ring_buffer.
+    // The elements are indexed with zero being the front cursor.
+    const value_type& at(size_type index) const
+    {
+        circular_index cursor = index_at(index);
+        return *((value_type *)_buffer + cursor);
+    }
+
+    // Returns the i'th element of the ring_buffer.
+    // The elements are indexed with zero being the front cursor.
+    value_type& operator[](size_type i)
+    {
+        return at(i);
+    }
+
+    // Returns the i'th element of the ring_buffer.
+    // The elements are indexed with zero being the front cursor.
+    const value_type& operator[](size_type i) const
+    {
+        return at(i);
+    }
+
+    // Pushes an item onto the back of the buffer.
     // Returns true if this was successful, and false otherwise.
-    // This may fail if the ring_buffer is full.
+    // This may fail if there is not enough space.
     bool push_back(value_type value)
     {
-        // Drop it on the floor if the _buffer is full.
+        // Drop it on the floor if the buffer is full.
         if (full()) {
             return false;
         }
 
-        new ((value_type *)_buffer + _enqueue_pos) value_type(value);
-        _enqueue_pos++;
-
-        if (_enqueue_pos >= CAPACITY) {
-            _enqueue_pos = 0;
+        if (!empty()) {
+            _back_pos++;
         }
 
         _count++;
+        new ((value_type *)_buffer + _back_pos) value_type(value);
 
         return true;
     }
 
-    // Dequeues and returns the next item in the ring_buffer.
+    // Pushes an item onto the front of the buffer.
+    // Returns true if this was successful, and false otherwise.
+    // This may fail if there is not enough space.
+    bool push_front(value_type value)
+    {
+        // Drop it on the floor if the buffer is full.
+        if (full()) {
+            return false;
+        }
+
+        if (!empty()) {
+            _front_pos--;
+        }
+
+        _count++;
+        new ((value_type *)_buffer + _front_pos) value_type(value);
+
+        return true;
+    }
+
+    // Pops an item off the back of the ring_buffer, and returns it.
+    // This cannot be called if the ring_buffer is empty.
+    value_type pop_back()
+    {
+        assert(!empty());
+
+        value_type *ptr = (value_type *)_buffer + _back_pos;
+        value_type value = std::move(*ptr);
+        ptr->~value_type();
+        _back_pos--;
+        _count--;
+
+        return value;
+    }
+
+    // Pops an item off the front of the ring_buffer, and returns it.
     // This cannot be called if the ring_buffer is empty.
     value_type pop_front()
     {
         assert(!empty());
 
-        _count--;
-
-        value_type *ptr = (value_type *)_buffer + _dequeue_pos;
+        value_type *ptr = (value_type *)_buffer + _front_pos;
         value_type value = std::move(*ptr);
         ptr->~value_type();
-        _dequeue_pos++;
-
-        if(_dequeue_pos >= CAPACITY) {
-            _dequeue_pos = 0;
-        }
+        _front_pos++;
+        _count--;
 
         return value;
     }
--- Common/test/test_ring_buffer.cpp
+++ Common/test/test_ring_buffer.cpp
@@ -1,6 +1,9 @@
 #include "catch.hpp"
 
 #include <common/ring_buffer.hpp>
+#include <cstdio>
+
+#define VERBOSE 0
 
 TEST_CASE("Initialize an empty buffer", "[ring_buffer]")
 {
@@ -13,61 +16,172 @@ TEST_CASE("Initialize an empty buffer", "[ring_buffer]")
 
 static int count_ctor = 0, count_ctor_move = 0, count_ctor_copy = 0, count_dtor = 0;
 
-TEST_CASE("Enqueue until full, dequeue until empty", "[ring_buffer]")
+class whatever {
+    int value;
+
+public:
+
+    ~whatever()
+    {
+        value = -2; // This value lets us see that we're accessing a destructed object.
+        count_dtor++;
+    }
+
+    whatever(int x) : value(x)
+    {
+        count_ctor++;
+    }
+
+    whatever(whatever &&other) : value(other.value)
+    {
+        other.value = -1; // This value lets us see that we're accessing a moved-from object.
+        count_ctor_move++;
+    }
+
+    whatever(const whatever &other) : value(other.value)
+    {
+        count_ctor_copy++;
+    }
+
+    operator int() const
+    {
+        return value;
+    }
+};
+
+#if VERBOSE
+template<typename T>
+static void print_buffer(const char *tag, const T &buffer)
 {
-    class whatever {
-        int value;
+    ::printf("%s\t-->\t[ ", tag);
+    for (size_t i = 0; i < buffer.size(); ++i) {
+        ::printf("%d%s",
+                 (int)buffer[i],
+                 (i < (buffer.size()-1)) ? ", " : " ");
+    }
+    ::printf("]\n");
+}
+#else
+template<typename T>
+static void print_buffer(const char *, const T &) {}
+#endif
 
-    public:
-
-        ~whatever()
-        {
-            count_dtor++;
-        }
-
-        whatever(int x) : value(x)
-        {
-            count_ctor++;
-        }
-
-        whatever(whatever &&other) : value(other.value)
-        {
-            other.value = -1;
-            count_ctor_move++;
-        }
-
-        whatever(const whatever &other) : value(other.value)
-        {
-            count_ctor_copy++;
-        }
-
-        operator int() const
-        {
-            return value;
-        }
-    };
-
+TEST_CASE("Push back, pop front", "[ring_buffer]")
+{
     {
         ring_buffer<whatever, 3> buffer;
 
-        // Enqueue until full. Dequeue until empty.
-        // Do this a few times to ensure we cross the internal buffer's boundary.
-        for (size_t i = 0; i < 3; ++i) {
-            REQUIRE(buffer.push_back(0));
-            REQUIRE(buffer.push_back(1));
-            REQUIRE(buffer.push_back(2));
-            REQUIRE(!buffer.push_back(3));
-            REQUIRE(buffer.full());
-            REQUIRE(buffer.size() == buffer.capacity());
+        REQUIRE(buffer.push_back(0));
+        print_buffer("buffer.push_back(0)", buffer);
+        // [ 0 ]
+        REQUIRE(buffer.front() == 0);
+        REQUIRE(buffer.back() == 0);
 
-            REQUIRE(buffer.pop_front() == 0);
-            REQUIRE(buffer.pop_front() == 1);
-            REQUIRE(buffer.pop_front() == 2);
-            REQUIRE(buffer.empty());
-            REQUIRE(buffer.size() == 0);
-        }
+        REQUIRE(buffer.push_back(1));
+        print_buffer("buffer.push_back(1)", buffer);
+        // [ 0, 1 ]
+        REQUIRE(buffer.front() == 0);
+        REQUIRE(buffer.back() == 1);
+
+        REQUIRE(buffer.push_back(2));
+        print_buffer("buffer.push_back(2)", buffer);
+        // [ 0, 1, 2 ]
+        REQUIRE(buffer.front() == 0);
+        REQUIRE(buffer.back() == 2);
+
+        REQUIRE(!buffer.push_back(3));
+        REQUIRE(buffer.full());
+        REQUIRE(buffer.size() == buffer.capacity());
+
+        REQUIRE(buffer.pop_front() == 0);
+        print_buffer("buffer.pop_front()", buffer);
+        // [ 1, 2 ]
+        REQUIRE(buffer.front() == 1);
+        REQUIRE(buffer.back() == 2);
+
+        REQUIRE(buffer.pop_front() == 1);
+        print_buffer("buffer.pop_front()", buffer);
+        // [ 2 ]
+        REQUIRE(buffer.front() == 2);
+        REQUIRE(buffer.back() == 2);
+
+        REQUIRE(buffer.pop_front() == 2);
+        print_buffer("buffer.pop_front()", buffer);
+        // [ ]
+        REQUIRE(buffer.empty());
+        REQUIRE(buffer.size() == 0);
     }
 
     // Make sure destructors get called for each element.
     REQUIRE((count_ctor + count_ctor_move + count_ctor_copy) == count_dtor);
+}
+
+TEST_CASE("Push front", "[ring_buffer]")
+{
+    ring_buffer<whatever, 3> buffer;
+    REQUIRE(buffer.push_front(0));
+    print_buffer("buffer.push_front(0)", buffer);
+    // [ 0 ]
+    REQUIRE(buffer.size() == 1);
+    REQUIRE(buffer.front() == 0);
+    REQUIRE(buffer.back() == 0);
+
+    REQUIRE(buffer.push_front(1));
+    print_buffer("buffer.push_front(1)", buffer);
+    // [ 1, 0 ]
+    REQUIRE(buffer.size() == 2);
+    REQUIRE(buffer.front() == 1);
+    REQUIRE(buffer.back() == 0);
+
+    REQUIRE(buffer.push_front(2));
+    print_buffer("buffer.push_front(2)", buffer);
+    // [ 2, 1, 0 ]
+    REQUIRE(buffer.size() == 3);
+    REQUIRE(buffer.front() == 2);
+    REQUIRE(buffer.back() == 0);
+}
+
+TEST_CASE("Push front, pop back", "[ring_buffer]")
+{
+    ring_buffer<whatever, 3> buffer;
+
+    REQUIRE(buffer.push_front(0));
+    print_buffer("buffer.push_front(0)", buffer);
+    // [ 0 ]
+    REQUIRE(buffer.front() == 0);
+    REQUIRE(buffer.back() == 0);
+
+    REQUIRE(buffer.push_front(1));
+    print_buffer("buffer.push_front(1)", buffer);
+    // [ 0, 1 ]
+    REQUIRE(buffer.front() == 1);
+    REQUIRE(buffer.back() == 0);
+
+    REQUIRE(buffer.push_front(2));
+    print_buffer("buffer.push_front(2)", buffer);
+    // [ 0, 1, 2 ]
+    REQUIRE(buffer.front() == 2);
+    REQUIRE(buffer.back() == 0);
+
+    REQUIRE(!buffer.push_front(3));
+    REQUIRE(buffer.full());
+    REQUIRE(buffer.size() == buffer.capacity());
+
+    REQUIRE(buffer.pop_back() == 0);
+    print_buffer("buffer.pop_back()", buffer);
+    // [ 1, 2 ]
+    REQUIRE(buffer.front() == 2);
+    REQUIRE(buffer.back() == 1);
+
+    REQUIRE(buffer.pop_back() == 1);
+    print_buffer("buffer.pop_back()", buffer);
+    // [ 2 ]
+    REQUIRE(buffer.front() == 2);
+    REQUIRE(buffer.back() == 2);
+
+    REQUIRE(buffer.pop_back() == 2);
+    print_buffer("buffer.pop_back()", buffer);
+    // [ ]
+    REQUIRE(buffer.empty());
+    REQUIRE(buffer.size() == 0);
 }
