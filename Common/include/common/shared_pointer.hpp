114101e7be2d7a99dbea3772fdbb2fa735dda204
foxostro <foxostro@gmail.com>
2018-09-11 00:58:49 -0700

    Reimplement perform_with_lock. This time with no internal compiler errors.
    With the new version of GCC I'm using in my cross compiler toolchain, this
    function no longer causes an internal compiler error.

--- Common/include/common/shared_pointer.hpp
+++ Common/include/common/shared_pointer.hpp
@@ -23,21 +23,21 @@ public:
 
     SharedPointer(const SharedPointer& other)
     {
-        other.lock_.lock();
-        data_ = other.data_;
-        count_ = other.count_;
-        acquire_unlocked();
-        other.lock_.unlock();
+        perform_with_lock(other.lock_, [&]{
+            data_ = other.data_;
+            count_ = other.count_;
+            acquire_unlocked();
+        });
     }
 
     SharedPointer(SharedPointer&& other)
     {
-        other.lock_.lock();
-        data_ = other.data_;
-        count_ = other.count_;
-        other.data_ = nullptr;
-        other.count_ = nullptr;
-        other.lock_.unlock();
+        perform_with_lock(other.lock_, [&]{
+            data_ = other.data_;
+            count_ = other.count_;
+            other.data_ = nullptr;
+            other.count_ = nullptr;
+        });
     }
 
     Type& operator*()
@@ -63,34 +63,30 @@ public:
     Type* get_pointer()
     {
         Type* data;
-        lock_.lock();
-        data = data_;
-        lock_.unlock();
+        perform_with_lock(lock_, [&]{
+            data = data_;
+        });
         return data;
     }
 
     const Type* get_pointer() const
     {
         Type* data;
-        lock_.lock();
-        data = data_;
-        lock_.unlock();
+        perform_with_lock(lock_, [&]{
+            data = data_;
+        });
         return data;
     }
 
     SharedPointer& operator=(const SharedPointer& other)
     {
         if (this != &other) {
-            lock_.lock();
-            other.lock_.lock();
-
-            release_unlocked();
-            data_ = other.data_;
-            count_ = other.count_;
-            acquire_unlocked();
-
-            lock_.unlock();
-            other.lock_.unlock();
+            perform_with_lock(lock_, other.lock_, [&]{
+                release_unlocked();
+                data_ = other.data_;
+                count_ = other.count_;
+                acquire_unlocked();
+            });
         }
         return *this;
     }
@@ -98,17 +94,13 @@ public:
     SharedPointer& operator=(SharedPointer&& other)
     {
         if (this != &other) {
-            lock_.lock();
-            other.lock_.lock();
-
-            release_unlocked();
-            data_ = other.data_;
-            count_ = other.count_;
-            other.data_ = nullptr;
-            other.count_ = nullptr;
-
-            lock_.unlock();
-            other.lock_.unlock();
+            perform_with_lock(lock_, other.lock_, [&]{
+                release_unlocked();
+                data_ = other.data_;
+                count_ = other.count_;
+                other.data_ = nullptr;
+                other.count_ = nullptr;
+            });
         }
         return *this;
     }
@@ -120,11 +112,9 @@ public:
         }
 
         bool is_equal;
-        lock_.lock();
-        other.lock_.lock();
-        is_equal = (data_ == other.data_);
-        lock_.unlock();
-        other.lock_.unlock();
+        perform_with_lock(lock_, other.lock_, [&]{
+            is_equal = (data_ == other.data_);
+        });
         return is_equal;
     }
 
@@ -136,18 +126,18 @@ public:
     operator bool() const
     {
         bool is_nonnull;
-        lock_.lock();
-        is_nonnull = (data_ != nullptr);
-        lock_.unlock();
+        perform_with_lock(lock_, [&]{
+            is_nonnull = (data_ != nullptr);
+        });
         return is_nonnull;
     }
 
     int get_count() const
     {
         int count;
-        lock_.lock();
-        count = (count_ ? count_->get_value() : 0);
-        lock_.unlock();
+        perform_with_lock(lock_, [&]{
+            count = (count_ ? count_->get_value() : 0);
+        });
         return count;
     }
     
--- Common/include/common/spin_lock.hpp
+++ Common/include/common/spin_lock.hpp
@@ -24,4 +24,22 @@ private:
     int lock_;
 };
 
+template<typename LockType, typename Function>
+inline void perform_with_lock(LockType& lock, Function&& fn)
+{
+    lock.lock();
+    fn();
+    lock.unlock();
+}
+
+template<typename LockType, typename Function>
+inline void perform_with_lock(LockType& lock1, LockType& lock2, Function&& fn)
+{
+    lock1.lock();
+    lock2.lock();
+    fn();
+    lock2.unlock();
+    lock1.unlock();
+}
+
 #endif // FLAPJACKOS_COMMON_INCLUDE_SPIN_LOCK_HPP
