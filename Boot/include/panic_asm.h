0ec474eb232d04bf8e0aebe5458f768d51c39725
foxostro <foxostro@gmail.com>
2018-10-02 22:20:49 -0700

    Boot code checks that the kernel fits into 4MB.

---
+++ Boot/include/panic_asm.h
@@ -0,0 +1,28 @@
+#ifndef FLAPJACKOS_BOOT_INCLUDE_PANIC_ASM_H
+#define FLAPJACKOS_BOOT_INCLUDE_PANIC_ASM_H
+
+#ifndef __ASSEMBLER__
+#error "This header is intended for use only in assembly code."
+#endif
+
+.macro PANIC STRING
+leal 0xB8000, %edi
+leal \STRING, %esi
+panic_print_loop:
+xor %eax, %eax
+movb (%esi), %ax
+cmpb $0, %ax
+je panic_halt_loop
+movl %eax, (%edi)
+incl %edi
+movl $0b00000111, (%edi)
+incl %edi
+incl %esi
+jmp panic_print_loop
+panic_halt_loop:
+cli
+hlt
+jmp panic_halt_loop
+.endm
+
+#endif // FLAPJACKOS_BOOT_INCLUDE_PANIC_ASM_H
