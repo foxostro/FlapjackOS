dba0152faa658b34810bfe3a6def31f978acc1ec
foxostro <foxostro@gmail.com>
2018-11-27 23:23:43 -0800

    Ensure kernel image info symbols are defined while tests are running.

--- Kernel/test/test_main.cpp
+++ Kernel/test/test_main.cpp
@@ -1,2 +1,20 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
+#include "boot.h"
+
+extern "C" {
+
+char g_kernel_physical_start[1] = {'x'};
+char g_boot_end[1] = {'x'};
+char g_kernel_image_begin[1] = {'x'};
+char g_kernel_text_begin[1] = {'x'};
+char g_kernel_text_end[1] = {'x'};
+char g_kernel_rodata_begin[1] = {'x'};
+char g_kernel_rodata_end[1] = {'x'};
+char g_start_ctors[1] = {'x'};
+char g_end_ctors[1] = {'x'};
+char g_kernel_data_end[1] = {'x'};
+char g_kernel_image_end[1] = {'x'};
+char g_kernel_physical_end[1] = {'x'};
+
+} // extern "C"
