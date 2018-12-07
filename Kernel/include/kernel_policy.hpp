4a4eae92409c622e60c4bf6d5ec7c4ac3f501185
foxostro <foxostro@gmail.com>
2018-12-07 07:44:11 -0800

    The KernelPolicy uses the new x86_64 implementations of classes.
    Now using x86_64::KernelAddressSpaceBootstrapper and PhysicalMemoryMap.

--- Kernel/include/kernel_policy.hpp
+++ Kernel/include/kernel_policy.hpp
@@ -6,15 +6,15 @@
 // TODO: Write platform-specific MMU classes for x86_64.
 #include <platform/x86_64/hardware_task_configuration.hpp>
 #include <platform/i386/hardware_interrupt_controller.hpp>
-#include <platform/i386/kernel_address_space_bootstrapper.hpp>
-#include <platform/i386/physical_memory_map.hpp>
+#include <platform/x86_64/kernel_address_space_bootstrapper.hpp>
+#include <platform/x86_64/physical_memory_map.hpp>
 #include <drivers/pc/vga_text_display_device.hpp>
 
 struct KernelPolicy {
 	using HardwareTaskConfiguration = x86_64::HardwareTaskConfiguration;
 	using HardwareInterruptController = i386::HardwareInterruptController;
-	using KernelAddressSpaceBootstrapper = i386::KernelAddressSpaceBootstrapper;
-	using PhysicalMemoryMap = i386::PhysicalMemoryMap;
+	using KernelAddressSpaceBootstrapper = x86_64::KernelAddressSpaceBootstrapper;
+	using PhysicalMemoryMap = x86_64::PhysicalMemoryMap;
 	using TextDisplayDevice = VGATextDisplayDevice;
 };
 
