0a01ab870642ce730d805a796cc0c7cdfe78ab7d
foxostro <foxostro@gmail.com>
2018-12-07 08:13:20 -0800

    Add stubs for x86_64::set_cr2() and x86_64::set_cr3() functions.

--- KernelPlatformSupport/include/platform/x86_64/creg.hpp
+++ KernelPlatformSupport/include/platform/x86_64/creg.hpp
@@ -1,9 +1,14 @@
 #ifndef FLAPJACKOS_KERNELPLATFORMSUPPORT_INCLUDE_PLATFORM_X86_64_CREG_HPP
 #define FLAPJACKOS_KERNELPLATFORMSUPPORT_INCLUDE_PLATFORM_X86_64_CREG_HPP
 
+#include <cstdint>
+#include <cassert>
+
 extern "C" {
 uint64_t x86_64_get_cr2(void);
-uint64_t x86_64_get_cr2(void);
+uint64_t x86_64_get_cr3(void);
+void x86_64_set_cr2(uint64_t value);
+void x86_64_set_cr3(uint64_t value);
 } // extern "C"
 
 namespace x86_64 {
@@ -21,8 +26,20 @@ namespace x86_64 {
 #       ifdef TESTING
     	return 0;
 #       else
-        return x86_64_get_cr2();
+        return x86_64_get_cr3();
 #       endif
+    }
+
+    inline void set_cr2(uint64_t value)
+    {
+        (void)value;
+        assert(!"set_cr2 is a stub");
+    }
+
+    inline void set_cr3(uint64_t value)
+    {
+        (void)value;
+        assert(!"set_cr3 is a stub");
     }
 } // namespace x86_64
 
