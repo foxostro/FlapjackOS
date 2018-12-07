35260d2721a359104ff530d6eb3f0bf726ca95c4
foxostro <foxostro@gmail.com>
2018-12-07 08:20:31 -0800

    Add i386::HardwareMemoryManagementUnit.

---
+++ KernelPlatformSupport/include/platform/i386/hardware_memory_management_unit.hpp
@@ -0,0 +1,19 @@
+#ifndef FLAPJACKOS_KERNELPLATFORMSUPPORT_INCLUDE_PLATFORM_I386_HARDWARE_MEMORY_MANAGEMENT_UNIT_HPP
+#define FLAPJACKOS_KERNELPLATFORMSUPPORT_INCLUDE_PLATFORM_I386_HARDWARE_MEMORY_MANAGEMENT_UNIT_HPP
+
+#include <cstdint>
+#include "creg.hpp"
+
+namespace i386 {
+
+class HardwareMemoryManagementUnit {
+public:
+    uint32_t get_cr3()
+    {
+        return i386::get_cr3();
+    }
+};
+
+} // namespace i386
+
+#endif // FLAPJACKOS_KERNELPLATFORMSUPPORT_INCLUDE_PLATFORM_I386_HARDWARE_MEMORY_MANAGEMENT_UNIT_HPP
