5f754a464723ac70d27c54c3095e4a43abd05212
foxostro <foxostro@gmail.com>
2018-12-08 16:29:51 -0800

    Unit tests are split into a 32-bit test suite and a 64-bit test suite.
    This fixes problems where code written to expect a 32-bit environment
    could not run in a 64-bit test runner, and vice-versa. For example, it is
    not possible to stash a pointer in an i386 PDE and follow it to the page
    table when the test suite is running in a 64-bit environment.

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,7 +4,8 @@ set -e
 BUILD_PREFIX="build/"
 X86_64_BUILD_DIR="$BUILD_PREFIX/x86_64"
 I386_BUILD_DIR="$BUILD_PREFIX/i386"
-TEST_BUILD_DIR="$BUILD_PREFIX/test"
+TEST32_BUILD_DIR="$BUILD_PREFIX/test_i386"
+TEST64_BUILD_DIR="$BUILD_PREFIX/test_x86_64"
 
 if [ ! -d "$X86_64_BUILD_DIR" ]; then
     cmake -E make_directory "$X86_64_BUILD_DIR"
@@ -16,11 +17,17 @@ if [ ! -d "$I386_BUILD_DIR" ]; then
     cmake -E chdir "$I386_BUILD_DIR" cmake ../.. -DCMAKE_TOOLCHAIN_FILE="i386-elf.toolchain.cmake"
 fi
 
-if [ ! -d "$TEST_BUILD_DIR" ]; then
-    cmake -E make_directory "$TEST_BUILD_DIR"
-    cmake -E chdir "$TEST_BUILD_DIR" cmake ../..
+if [ ! -d "$TEST32_BUILD_DIR" ]; then
+    cmake -E make_directory "$TEST32_BUILD_DIR"
+    cmake -E chdir "$TEST32_BUILD_DIR" cmake ../.. -DBUILD_32_BIT_TESTS=ON
+fi
+
+if [ ! -d "$TEST64_BUILD_DIR" ]; then
+    cmake -E make_directory "$TEST64_BUILD_DIR"
+    cmake -E chdir "$TEST64_BUILD_DIR" cmake ../.. -DBUILD_32_BIT_TESTS=OFF
 fi
 
 cmake --build "$X86_64_BUILD_DIR" || echo "ERROR: x86_64 build failed."
 cmake --build "$I386_BUILD_DIR" || echo "ERROR: i386 build failed."
-cmake --build "$TEST_BUILD_DIR" || echo "ERROR: unit test build failed."
+cmake --build "$TEST32_BUILD_DIR" || echo "ERROR: i386 unit test build failed."
+cmake --build "$TEST64_BUILD_DIR" || echo "ERROR: x86_64 unit test build failed."
--- scripts/test.sh
+++ scripts/test.sh
@@ -2,14 +2,28 @@
 set -e
 
 BUILD_PREFIX="build/"
-TEST_BUILD_DIR="$BUILD_PREFIX/test"
+TEST32_BUILD_DIR="$BUILD_PREFIX/test_i386"
+TEST64_BUILD_DIR="$BUILD_PREFIX/test_x86_64"
 
-if [ ! -d "$TEST_BUILD_DIR" ]; then
-    cmake -E make_directory "$TEST_BUILD_DIR"
-    cmake -E chdir "$TEST_BUILD_DIR" cmake ../..
+if [ ! -d "$TEST32_BUILD_DIR" ]; then
+    cmake -E make_directory "$TEST32_BUILD_DIR"
+    cmake -E chdir "$TEST32_BUILD_DIR" cmake ../.. -DBUILD_32_BIT_TESTS=ON
 fi
 
-cmake --build "$TEST_BUILD_DIR"
+if [ ! -d "$TEST64_BUILD_DIR" ]; then
+    cmake -E make_directory "$TEST64_BUILD_DIR"
+    cmake -E chdir "$TEST64_BUILD_DIR" cmake ../.. -DBUILD_32_BIT_TESTS=OFF
+fi
 
-cd "$TEST_BUILD_DIR"
+cmake --build "$TEST32_BUILD_DIR"
+cmake --build "$TEST64_BUILD_DIR"
+
+pushd "$TEST32_BUILD_DIR"
+echo "Running 32-bit unit tests."
 ctest --output-on-failure
+popd
+
+pushd "$TEST64_BUILD_DIR"
+echo "Running 64-bit unit tests."
+ctest --output-on-failure
+popd
