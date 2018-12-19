2db8ebec63a859624b1b38770a3fc749d3935560
foxostro <foxostro@gmail.com>
2018-12-18 22:09:27 -0800

    get_interrupt_name method moved to platform-specific code
    The implementation of this method is platform-specific and should not be
    on the platform-agnostic, generic class.

--- Kernel/include/generic_interrupt_dispatcher.hpp
+++ Kernel/include/generic_interrupt_dispatcher.hpp
@@ -75,6 +75,9 @@ public:
         return should_panic;
     }
 
+protected:
+    virtual const char* get_interrupt_name(unsigned interrupt_number) = 0;
+
 private:
     LockType lock_;
     Handler handlers_[IDT_MAX];
@@ -91,35 +94,6 @@ private:
         panic("No handler for interrupt \"%s\" (%u)\n",
             get_interrupt_name(interrupt_number),
             interrupt_number);
-    }
-
-    // AFOX_TODO: This method is platform-specific and probably doesn't belong on the GenericInterruptDispatcher class. Maybe move to a platform-specific subclass?
-    const char* get_interrupt_name(unsigned interrupt_number) noexcept
-    {
-        switch (interrupt_number) {
-            case IDT_KEY:   return "Keyboard";
-            case IDT_TIMER: return "Timer";
-            case IDT_DE:    return "Division Error";
-            case IDT_DB:    return "Debug Exception";
-            case IDT_NMI:   return "Non-Maskable Interrupt";
-            case IDT_BP:    return "Breakpoint";
-            case IDT_OF:    return "Overflow";
-            case IDT_BR:    return "BOUND Range exceeded";
-            case IDT_UD:    return "Undefined Opcode";
-            case IDT_NM:    return "No Math coprocessor";
-            case IDT_DF:    return "Double Fault.";
-            case IDT_CSO:   return "Coprocessor Segment Overrun";
-            case IDT_TS:    return "Invalid Task Segment Selector.";
-            case IDT_NP:    return "Segment Not Present.";
-            case IDT_SS:    return "Stack Segment Fault.";
-            case IDT_GP:    return "General Protection Fault.";
-            case IDT_PF:    return "Page Fault";
-            case IDT_MF:    return "X87 Math Fault";
-            case IDT_AC:    return "Alignment Check";
-            case IDT_MC:    return "Machine Check";
-            case IDT_XF:    return "SSE Floating Point Exception";
-            default:        return "unknown";
-        };
     }
 };
 
