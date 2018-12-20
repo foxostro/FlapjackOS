edd28ea639096ed9e618c468265a255b2019b229
foxostro <foxostro@gmail.com>
2018-12-20 01:49:25 -0800

    x86_64::PanicInterruptHandler

--- Kernel/src/platform/x86_64/panic_interrupt_handler.cpp
+++ Kernel/src/platform/x86_64/panic_interrupt_handler.cpp
@@ -2,6 +2,7 @@
 #include <panic.h>
 #include <common/minmax.hpp>
 #include <cstring>
+#include <cstdio>
 
 namespace x86_64 {
 
@@ -14,10 +15,102 @@ PanicInterruptHandler::PanicInterruptHandler(const char *message, bool error_cod
 
 void PanicInterruptHandler::int_handler(const InterruptParameters& params) noexcept
 {
-    // AFOX_TODO: STUB.
-    (void)params;
-    (void)error_code_present_;
-    panic(message_);
+    // This is a lot of characters. We need to ensure this memory is allocated
+    // in .data and specifically NOT on the stack. Do this by marking it as
+    // static. We certainly don't need to worry about reentrancy here since
+    // 1) we're going to panic, and 2) we are in an interrupt context.
+    static char text[1024] = {0};
+
+    if (error_code_present_) {
+        snprintf(text, sizeof(text),
+                "Registers:\n"
+                "fs = 0x%x\n"
+                "gs = 0x%x\n"
+                "r15 = %p\n"
+                "r14 = %p\n"
+                "r13 = %p\n"
+                "r12 = %p\n"
+                "r11 = %p\n"
+                "r10 = %p\n"
+                "r9 = %p\n"
+                "r8 = %p\n"
+                "rdi = %p\n"
+                "rsi = %p\n"
+                "rbp = %p\n"
+                "rsp = %p\n"
+                "rbx = %p\n"
+                "rdx = %p\n"
+                "rcx = %p\n"
+                "rax = %p\n"
+                "rip = %p\n"
+                "Error Code: 0x%x\n"
+                "%s",
+                params.fs,
+                params.gs,
+                (void*)(uintptr_t)params.r15,
+                (void*)(uintptr_t)params.r14,
+                (void*)(uintptr_t)params.r13,
+                (void*)(uintptr_t)params.r12,
+                (void*)(uintptr_t)params.r11,
+                (void*)(uintptr_t)params.r10,
+                (void*)(uintptr_t)params.r9,
+                (void*)(uintptr_t)params.r8,
+                (void*)(uintptr_t)params.rdi,
+                (void*)(uintptr_t)params.rsi,
+                (void*)(uintptr_t)params.rbp,
+                (void*)(uintptr_t)params.rsp,
+                (void*)(uintptr_t)params.rbx,
+                (void*)(uintptr_t)params.rdx,
+                (void*)(uintptr_t)params.rcx,
+                (void*)(uintptr_t)params.rax,
+                (void*)(uintptr_t)params.rip,
+                (unsigned)params.error_code,
+                message_);
+    } else {
+        snprintf(text, sizeof(text),
+                "Registers:\n"
+                "fs = 0x%x\n"
+                "gs = 0x%x\n"
+                "r15 = %p\n"
+                "r14 = %p\n"
+                "r13 = %p\n"
+                "r12 = %p\n"
+                "r11 = %p\n"
+                "r10 = %p\n"
+                "r9 = %p\n"
+                "r8 = %p\n"
+                "rdi = %p\n"
+                "rsi = %p\n"
+                "rbp = %p\n"
+                "rsp = %p\n"
+                "rbx = %p\n"
+                "rdx = %p\n"
+                "rcx = %p\n"
+                "rax = %p\n"
+                "rip = %p\n"
+                "%s",
+                params.fs,
+                params.gs,
+                (void*)(uintptr_t)params.r15,
+                (void*)(uintptr_t)params.r14,
+                (void*)(uintptr_t)params.r13,
+                (void*)(uintptr_t)params.r12,
+                (void*)(uintptr_t)params.r11,
+                (void*)(uintptr_t)params.r10,
+                (void*)(uintptr_t)params.r9,
+                (void*)(uintptr_t)params.r8,
+                (void*)(uintptr_t)params.rdi,
+                (void*)(uintptr_t)params.rsi,
+                (void*)(uintptr_t)params.rbp,
+                (void*)(uintptr_t)params.rsp,
+                (void*)(uintptr_t)params.rbx,
+                (void*)(uintptr_t)params.rdx,
+                (void*)(uintptr_t)params.rcx,
+                (void*)(uintptr_t)params.rax,
+                (void*)(uintptr_t)params.rip,
+                message_);
+    }
+    panic(text);
 }
 
 } // namespace x86_64
