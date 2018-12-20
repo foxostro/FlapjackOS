2c2e20179fa3da2b3bb5e970c192855fcd68f407
foxostro <foxostro@gmail.com>
2018-12-20 00:51:30 -0800

    x86_64::HardwareInterruptController

--- Kernel/include/platform/x86_64/hardware_interrupt_controller.hpp
+++ Kernel/include/platform/x86_64/hardware_interrupt_controller.hpp
@@ -1,7 +1,11 @@
 #ifndef FLAPJACKOS_KERNEL_INCLUDE_PLATFORM_X86_64_HARDWARE_INTERRUPT_CONTROLLER_HPP
 #define FLAPJACKOS_KERNEL_INCLUDE_PLATFORM_X86_64_HARDWARE_INTERRUPT_CONTROLLER_HPP
 
+#include "idt.hpp"
+
 namespace x86_64 {
+
+// AFOX_TODO: There's a lot of similarities between the i386 and x86_64 HardwareInterruptController classes. Can these two be consolidated?
 
 class HardwareInterruptController {
 public:
@@ -10,6 +14,12 @@ public:
     void disable_interrupts();
     void enable_interrupts();
     bool clear(unsigned interrupt_number);
+
+private:
+    InterruptDescriptorTable idt_;
+
+    void build_idt();
+    void build_idt_entry(InterruptDescriptor& entry, uintptr_t offset);
 };
 
 } // namespace x86_64
