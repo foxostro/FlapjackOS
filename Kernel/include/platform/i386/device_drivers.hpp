ac56c23c009b07a52e619d51d0efb5fdb9b507bb
foxostro <foxostro@gmail.com>
2018-12-21 22:51:45 -0800

    Consolidate i386 and x86_64 implementations of DeviceDrivers class.

--- Kernel/include/platform/i386/device_drivers.hpp
+++ Kernel/include/platform/i386/device_drivers.hpp
@@ -2,16 +2,45 @@
 #define FLAPJACKOS_KERNEL_INCLUDE_PLATFORM_I386_DEVICE_DRIVERS_HPP
 
 #include <platform/i386/interrupt_controller.hpp>
+#include <platform/i386/interrupt_parameters.hpp>
 #include <drivers/pc/ps2_keyboard_device.hpp>
 #include <drivers/pc/pit_timer_device.hpp>
+#include <simple_device_interrupt_handler.hpp>
 
 namespace i386 {
 
-class DeviceDrivers {
+template<typename Policy>
+class GenericDeviceDrivers {
 public:
-    DeviceDrivers(InterruptController& interrupt_controller);
-    ~DeviceDrivers();
-    void init();
+    using InterruptParameters = typename Policy::InterruptParameters;
+    using InterruptController = typename Policy::InterruptController;
+
+    GenericDeviceDrivers(InterruptController& interrupt_controller)
+     : interrupt_controller_(interrupt_controller),
+       keyboard_device_(nullptr),
+       timer_device_(nullptr)
+    {}
+
+    ~GenericDeviceDrivers()
+    {
+        delete keyboard_device_; // AFOX_TODO: Need something like std::unique_ptr here. Or perhaps std::optional if these do not need to be heap allocated.
+        delete timer_device_;
+    }
+
+    void init()
+    {
+        // Keyboard
+        keyboard_device_ = new PS2KeyboardDevice();
+        auto keyboard_handler = new SimpleDeviceInterruptHandler<InterruptParameters, PS2KeyboardDevice>(*keyboard_device_);
+        interrupt_controller_.install(IDT_KEY, keyboard_handler);
+
+        // Timer
+        timer_device_ = new PITTimerDevice(PITTimerDevice::TIMER_RATE_10ms,
+                                        PITTimerDevice::TIMER_LEAP_INTERVAL_10ms,
+                                        PITTimerDevice::TIMER_LEAP_TICKS_10ms);
+        auto timer_handler = new SimpleDeviceInterruptHandler<InterruptParameters, PITTimerDevice>(*timer_device_);
+        interrupt_controller_.install(IDT_TIMER, timer_handler);
+    }
 
     inline KeyboardDevice& get_keyboard()
     {
@@ -24,6 +53,13 @@ private:
     PS2KeyboardDevice* keyboard_device_;
     PITTimerDevice* timer_device_;
 };
+
+struct DeviceDriversPolicy {
+    using InterruptParameters = i386::InterruptParameters;
+    using InterruptController = i386::InterruptController;
+};
+
+using DeviceDrivers = GenericDeviceDrivers<DeviceDriversPolicy>;
 
 } // namespace i386
 
--- Kernel/include/platform/x86_64/device_drivers.hpp
+++ Kernel/include/platform/x86_64/device_drivers.hpp
@@ -1,30 +1,17 @@
 #ifndef FLAPJACKOS_KERNEL_INCLUDE_PLATFORM_X86_64_DEVICE_DRIVERS_HPP
 #define FLAPJACKOS_KERNEL_INCLUDE_PLATFORM_X86_64_DEVICE_DRIVERS_HPP
 
+#include <platform/i386/device_drivers.hpp>
 #include <platform/x86_64/interrupt_controller.hpp>
-#include <drivers/pc/ps2_keyboard_device.hpp>
-#include <drivers/pc/pit_timer_device.hpp>
 
 namespace x86_64 {
 
-// TODO: This class is very similar to i386::DeviceDrivers. Can the two be consolidated?
-class DeviceDrivers {
-public:
-    DeviceDrivers(InterruptController& interrupt_controller);
-    ~DeviceDrivers();
-    void init();
-
-    inline KeyboardDevice& get_keyboard()
-    {
-        assert(keyboard_device_);
-        return *keyboard_device_;
-    }
-
-private:
-    InterruptController& interrupt_controller_;
-    PS2KeyboardDevice* keyboard_device_;
-    PITTimerDevice* timer_device_;
+struct DeviceDriversPolicy {
+    using InterruptParameters = x86_64::InterruptParameters;
+    using InterruptController = x86_64::InterruptController;
 };
+
+using DeviceDrivers = i386::GenericDeviceDrivers<DeviceDriversPolicy>;
 
 } // namespace x86_64
 
