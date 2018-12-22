18995c8db169a20267efdfd13dc48d8c54c13abe
foxostro <foxostro@gmail.com>
2018-12-22 00:13:22 -0800

    InterruptLock is a recursive lock.

--- Kernel/src/interrupt_lock.cpp
+++ Kernel/src/interrupt_lock.cpp
@@ -1,12 +1,23 @@
 #include <interrupt_lock.hpp>
-#include <kernel.hpp>
+#include <interrupt_asm.h>
+
+// The GCC freestanding implementation does not seem to provide support for
+// std::atomic. If it did then the counter below could be replaced with the
+// std::atomic<int>. This change would enable InterruptLock to work correctly
+// when we have preemption working in the kernel.
+// AFOX_TODO: Use an atomic counter so that InterruptLock will work with a preemptive kernel.
+static int g_interrupt_lock_count = 0;
 
 void InterruptLock::lock()
 {
-    get_global_kernel().disable_interrupts();
+    if (g_interrupt_lock_count++ == 0) {
+        ::disable_interrupts();
+    }
 }
 
 void InterruptLock::unlock()
 {
-    get_global_kernel().enable_interrupts();
+    if (g_interrupt_lock_count-- == 0) {
+        ::enable_interrupts();
+    }
 }
