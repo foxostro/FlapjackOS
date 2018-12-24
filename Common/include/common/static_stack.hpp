646ee38162a65ed7115f7fab8ca81c32355e74fe
foxostro <foxostro@gmail.com>
2018-12-24 00:46:17 -0800

    Additional StaticStack API

--- Common/include/common/static_stack.hpp
+++ Common/include/common/static_stack.hpp
@@ -8,12 +8,14 @@
 template<size_t StackSize>
 class StaticStack {
 public:
+    // Default ctor
     StaticStack()
     {
         memset(backing_, 0, sizeof(backing_));
         stack_pointer = backing_ + sizeof(backing_);
     }
 
+    // Copy ctor
     StaticStack(const StaticStack& other)
     {
         memmove(backing_, other.backing_, sizeof(backing_));
@@ -21,6 +23,7 @@ public:
         stack_pointer = backing_ + offset;
     }
 
+    // Move ctor
     StaticStack(StaticStack&& other)
     {
         memmove(backing_, other.backing_, sizeof(backing_));
@@ -28,11 +31,50 @@ public:
         stack_pointer = backing_ + offset;
     }
 
+    // Copy-assignment operator
+    StaticStack& operator=(const StaticStack &other)
+    {
+        if (this != &other) {
+            memmove(backing_, other.backing_, sizeof(backing_));
+            ptrdiff_t offset = other.stack_pointer - other.backing_;
+            stack_pointer = backing_ + offset;
+        }
+        return *this;
+    }
+
+    // Move-assignment operator
+    StaticStack& operator=(StaticStack &&other)
+    {
+        if (this != &other) {
+            memmove(backing_, other.backing_, sizeof(backing_));
+            ptrdiff_t offset = other.stack_pointer - other.backing_;
+            stack_pointer = backing_ + offset;
+        }
+        return *this;
+    }
+
     template<typename T>
     void push(T value)
     {
         stack_pointer -= sizeof(value);
         memcpy(stack_pointer, &value, sizeof(value));
+    }
+    
+    template<typename T>
+    T pop()
+    {
+        T result;
+        memcpy(&result, stack_pointer, sizeof(result));
+        stack_pointer += sizeof(result);
+        return result;
+    }
+    
+    template<typename T>
+    T peek(int delta_objects)
+    {
+        T result;
+        memcpy(&result, stack_pointer + sizeof(result)*delta_objects, sizeof(result));
+        return result;
     }
 
     char* stack_pointer;
