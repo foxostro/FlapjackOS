eea365464c7e1bb7dea00fde256ba9379147d481
foxostro <foxostro@gmail.com>
2018-12-23 20:23:49 -0800

    x86_64::Thread and context switch

--- Kernel/include/platform/x86_64/thread.hpp
+++ Kernel/include/platform/x86_64/thread.hpp
@@ -13,18 +13,26 @@ class Thread {
 public:
     Thread(void* instruction_pointer)
     {
-        stack_.push(/*EIP=*/reinterpret_cast<uintptr_t>(instruction_pointer));
-        char* EBP = stack_.stack_pointer - sizeof(EBP);
-        stack_.push(/*EBP=*/EBP);
-        char* ESP = stack_.stack_pointer;
-        stack_.push(/*POPA, EAX=*/InitialRegisterValue);
-        stack_.push(/*POPA, ECX=*/InitialRegisterValue);
-        stack_.push(/*POPA, EDX=*/InitialRegisterValue);
-        stack_.push(/*POPA, EBX=*/InitialRegisterValue);
-        stack_.push(/*POPA, ESP=*/ESP);
-        stack_.push(/*POPA, EBP=*/EBP);
-        stack_.push(/*POPA, ESI=*/InitialRegisterValue);
-        stack_.push(/*POPA, EDI=*/InitialRegisterValue);
+        stack_.push(/*RIP=*/reinterpret_cast<uintptr_t>(instruction_pointer));
+        char* RBP = stack_.stack_pointer - sizeof(RBP);
+        stack_.push(/*RBP=*/RBP);
+        char* RSP = stack_.stack_pointer;
+        stack_.push(/*RAX=*/InitialRegisterValue);
+        stack_.push(/*RCX=*/InitialRegisterValue);
+        stack_.push(/*RDX=*/InitialRegisterValue);
+        stack_.push(/*RBX=*/InitialRegisterValue);
+        stack_.push(/*RSP=*/RSP);
+        stack_.push(/*RBP=*/RBP);
+        stack_.push(/*RSI=*/InitialRegisterValue);
+        stack_.push(/*RDI=*/InitialRegisterValue);
+        stack_.push(/*R8=*/InitialRegisterValue);
+        stack_.push(/*R9=*/InitialRegisterValue);
+        stack_.push(/*R10=*/InitialRegisterValue);
+        stack_.push(/*R11=*/InitialRegisterValue);
+        stack_.push(/*R12=*/InitialRegisterValue);
+        stack_.push(/*R13=*/InitialRegisterValue);
+        stack_.push(/*R14=*/InitialRegisterValue);
+        stack_.push(/*R15=*/InitialRegisterValue);
     }
 
     void context_switch()
