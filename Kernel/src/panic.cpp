475dd0f9849b634f42136502c2aa19983a693676
foxostro <foxostro@gmail.com>
2018-12-26 20:47:25 -0800

    Prevent a context switch from occurring during a panic().

--- Kernel/src/panic.cpp
+++ Kernel/src/panic.cpp
@@ -11,17 +11,14 @@
 #include <cstdio>
 #include <cstdarg>
 
-extern TextTerminal *g_terminal; // defined in kernel.cpp
-
 constexpr size_t PANIC_BUFFER_SIZE = 1024;
 
 extern "C"
 __attribute__((noreturn))
 void panic(const char *fmt, ...)
 {
-    TRACE("Kernel is going to panic.");
-
     disable_interrupts();
+    TRACE("Kernel is going to panic.");
 
     char buffer[PANIC_BUFFER_SIZE];
     va_list args;
@@ -31,16 +28,12 @@ void panic(const char *fmt, ...)
 
     TRACE("message is \"%s\"", buffer);
 
-    if (g_terminal) {
-        g_terminal->printf("PANIC: %s\n", buffer);
-    } else {
-        // If the panic happened early in the boot process then we may not have
-        // a text terminal yet. In this case, panic() makes its own.
-        VGATextDisplayDevice display;
-        TextTerminal terminal;
-        terminal.init(&display);
-        terminal.printf("PANIC: %s\n", buffer);
-    }
+    // If the panic happened early in the boot process then we may not have
+    // a text terminal yet. In this case, panic() makes its own.
+    VGATextDisplayDevice display;
+    UnlockedTextTerminal terminal;
+    terminal.init(&display);
+    terminal.printf("PANIC: %s\n\n", buffer);
 
     halt_forever();
 }
