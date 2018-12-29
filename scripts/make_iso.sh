a6ec015c89d51807b43cfd7e9ad7929631fa2ac1
foxostro <foxostro@gmail.com>
2018-12-28 16:24:40 -0800

    Added User/ subdirectory for user programs.
    User programs are loaded as multiboot modules.

--- scripts/make_iso.sh
+++ scripts/make_iso.sh
@@ -3,7 +3,8 @@ set -e
 KERNEL_BIN="$1"
 GRUB_CFG="$2"
 ISO_FN="$3"
-MODULES_DIR="$4"
+MODULES_1="$4"
+MODULES_2="$5"
 
 grub-file --is-x86-multiboot "$KERNEL_BIN"
 if [[ $? -ne 0 ]]; then
@@ -14,9 +15,10 @@ fi
 
 ISODIR="$(mktemp -d)"
 mkdir -p "$ISODIR/boot/grub"
-mkdir -p "$ISODIR/modules"
 cp "$KERNEL_BIN" "$ISODIR/boot"
 cp "$GRUB_CFG" "$ISODIR/boot/grub/grub.cfg"
-ditto "$MODULES_DIR" "$ISODIR/modules"
+mkdir -p "$ISODIR/modules"
+cp "$MODULES_1" "$ISODIR/modules"
+cp "$MODULES_2" "$ISODIR/modules"
 grub-mkrescue -o "$ISO_FN" "$ISODIR"
 rm -rf "$ISODIR"
