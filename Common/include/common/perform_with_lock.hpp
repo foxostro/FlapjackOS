3b806c6bc784f0f793edde35ae3c5c6fb991e952
foxostro <foxostro@gmail.com>
2019-01-03 18:52:15 -0800

    Fix bug in perform_with_lock() that made it difficult to return a reference.

--- Common/include/common/perform_with_lock.hpp
+++ Common/include/common/perform_with_lock.hpp
@@ -3,16 +3,15 @@
 
 #include <common/lock_guard.hpp>
 
-// AFOX_TODO: perform_with_lock() doesn't really work when attempting to return a reference. I get errors like "error: returning reference to local temporary object [-Werror,-Wreturn-stack-address]". I think I can fix this with some magic involving forwarding references or universal references, but I'm not sure.
 template<typename LockType, typename Function>
-inline auto perform_with_lock(LockType& lock, Function&& fn)
+inline auto perform_with_lock(LockType& lock, Function&& fn) -> decltype(fn())
 {
     LockGuard guard{lock};
     return fn();
 }
 
 template<typename Lock1, typename Lock2, typename Function>
-inline auto perform_with_lock(Lock1& lock1, Lock2& lock2, Function&& fn)
+inline auto perform_with_lock(Lock1& lock1, Lock2& lock2, Function&& fn) -> decltype(fn())
 {
     LockGuard2 guard{lock1, lock2};
     return fn();
