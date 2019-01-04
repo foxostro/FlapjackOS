3b0813a7ab951bbad975788aa343d9119cc2bf82
foxostro <foxostro@gmail.com>
2019-01-03 19:08:43 -0800

    Add TODO notes

--- Common/include/common/perform_with_lock.hpp
+++ Common/include/common/perform_with_lock.hpp
@@ -10,9 +10,11 @@ inline auto perform_with_lock(LockType& lock, Function&& fn) -> decltype(fn())
     return fn();
 }
 
+// AFOX_TODO: Use variadic templates to generalize perform_with_lock() to N locks.
 template<typename Lock1, typename Lock2, typename Function>
 inline auto perform_with_lock(Lock1& lock1, Lock2& lock2, Function&& fn) -> decltype(fn())
 {
+    // AFOX_TODO: Use variadic templates to generalize LockGuard to N locks.
     LockGuard2 guard{lock1, lock2};
     return fn();
 }
