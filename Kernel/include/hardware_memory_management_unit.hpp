f4daf3647a4c1e021116d13329ab8be8dcd97dbc
foxostro <foxostro@gmail.com>
2019-01-04 17:22:57 -0800

    Added pure virtual "interface" for HardwareMemoryManagementUnit

---
+++ Kernel/include/hardware_memory_management_unit.hpp
@@ -0,0 +1,35 @@
+#ifndef FLAPJACKOS_KERNEL_INCLUDE_HARDWARE_MEMORY_MANAGEMENT_UNIT_HPP
+#define FLAPJACKOS_KERNEL_INCLUDE_HARDWARE_MEMORY_MANAGEMENT_UNIT_HPP
+
+#include <cstdint>
+
+// Interface for the hardware MMU.
+// Includes logic for converting between kernel logical addresses and physical
+// addresses because this is heavily dependent on the details of how the kernel
+// was mapped into the higher-half.
+// AFOX_TODO: This seems like a SRP violation. The class should be broken up.
+class HardwareMemoryManagementUnit {
+public:
+    // Gets the
+    // AFOX_TODO: Only x86 and x86_64 have CR3. So what how do I fit ARM into this API?
+    virtual uintptr_t get_cr3() const = 0;
+
+    // Reloads the hardware MMU and flushes the TLB.
+    virtual void reload() = 0;
+
+    // Convert the given physical address to a logical address.
+    // These addresses are easily converted between physical and virtual as they are
+    // always offset by KERNEL_VIRTUAL_START_ADDR.
+    virtual uintptr_t convert_physical_to_logical_address(uintptr_t physical_address) const = 0;
+
+    // Convert the given logical address to a physical address.
+    // These addresses are easily converted between physical and virtual as they are
+    // always offset by KERNEL_VIRTUAL_START_ADDR.
+    virtual uintptr_t convert_logical_to_physical_address(uintptr_t logical_address) const = 0;
+
+    // Gets the address of the start of the kernel virtual memory region.
+    // This is the start of the so-called higher-half.
+    virtual uintptr_t get_kernel_virtual_start_address() const = 0;
+};
+
+#endif // FLAPJACKOS_KERNEL_INCLUDE_HARDWARE_MEMORY_MANAGEMENT_UNIT_HPP
--- Kernel/include/platform/x86_64/concrete_hardware_memory_management_unit.hpp
+++ Kernel/include/platform/x86_64/concrete_hardware_memory_management_unit.hpp
@@ -1,20 +1,20 @@
 #ifndef FLAPJACKOS_KERNEL_INCLUDE_PLATFORM_X86_64_CONCRETE_HARDWARE_MEMORY_MANAGEMENT_UNIT_HPP
 #define FLAPJACKOS_KERNEL_INCLUDE_PLATFORM_X86_64_CONCRETE_HARDWARE_MEMORY_MANAGEMENT_UNIT_HPP
 
-#include <cstdint>
+#include <hardware_memory_management_unit.hpp>
 #include "creg.hpp"
 #include "kernel_image_info.hpp" // for KERNEL_VIRTUAL_START_ADDR
 
 namespace x86_64 {
 
-class ConcreteHardwareMemoryManagementUnit {
+class ConcreteHardwareMemoryManagementUnit final : public HardwareMemoryManagementUnit  {
 public:
-    uint64_t get_cr3()
+    uintptr_t get_cr3() const override
     {
         return x86_64::get_cr3();
     }
 
-    void reload()
+    void reload() override
     {
         x86_64::set_cr3(x86_64::get_cr3());
     }
@@ -22,7 +22,7 @@ public:
     // Convert the given physical address to a logical address.
     // These addresses are easily converted between physical and virtual as they are
     // always offset by KERNEL_VIRTUAL_START_ADDR.
-    uintptr_t convert_physical_to_logical_address(uint64_t physical_address)
+    uintptr_t convert_physical_to_logical_address(uintptr_t physical_address) const override
     {
         return physical_address + KERNEL_VIRTUAL_START_ADDR;
     }
@@ -30,7 +30,7 @@ public:
     // Convert the given logical address to a physical address.
     // These addresses are easily converted between physical and virtual as they are
     // always offset by KERNEL_VIRTUAL_START_ADDR.
-    uint64_t convert_logical_to_physical_address(uintptr_t logical_address)
+    uintptr_t convert_logical_to_physical_address(uintptr_t logical_address) const override
     {
         assert(logical_address >= KERNEL_VIRTUAL_START_ADDR);
         return logical_address - KERNEL_VIRTUAL_START_ADDR;
@@ -38,7 +38,7 @@ public:
 
     // Gets the address of the start of the kernel virtual memory region.
     // This is the start of the so-called higher-half.
-    constexpr uintptr_t get_kernel_virtual_start_address()
+    uintptr_t get_kernel_virtual_start_address() const override
     {
         return KERNEL_VIRTUAL_START_ADDR;
     }
