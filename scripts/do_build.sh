23f98896e959da104f780248527ad628116c9b30
foxostro <foxostro@gmail.com>
2019-01-09 01:26:27 -0800

    do_build.sh should exit with failure when cmake fails.

--- scripts/do_build.sh
+++ scripts/do_build.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
+set -e
 set -o pipefail
 BUILD_DIR="$1"
 cmake --build "$BUILD_DIR" \
     | sed -E 's/^\.\.\/\.\.\/(.*):/\1:/' \
     | GREP_COLOR='01;36' grep --color=always -E '^.*:\d+:\d+:|$' \
     | GREP_COLOR='01;31' grep --color=always -E 'error:|$' \
-    | GREP_COLOR='01;32' grep --color=always -E '\w*\^|$' \
-  || echo "ERROR: Build failed for \"$BUILD_DIR\"."
+    | GREP_COLOR='01;32' grep --color=always -E '\w*~*\^~*|$' \
+  || exit 1
