f54e5c6f13e205d4ce4ae60d2a4e0c9d33057ab7
foxostro <foxostro@gmail.com>
2019-01-09 01:26:48 -0800

    run.sh and test.sh should use output filtering wrapper too.

--- scripts/run.sh
+++ scripts/run.sh
@@ -12,7 +12,8 @@ CROSS_BUILD_DIR="$BUILD_PREFIX/$TARGET"
 CONFIGURE="./scripts/do_configure_build.sh"
 "$CONFIGURE" "$CROSS_BUILD_DIR" -DCMAKE_TOOLCHAIN_FILE="$TARGET-elf.toolchain.cmake"
 
-cmake --build "$CROSS_BUILD_DIR" -- -j8
+BUILD="./scripts/do_build.sh"
+"$BUILD" "$CROSS_BUILD_DIR"
 
 erb iso="$CROSS_BUILD_DIR/FlapjackOS.iso" bochsrc.erb > bochsrc
 echo > parport.out
--- scripts/test.sh
+++ scripts/test.sh
@@ -9,8 +9,9 @@ CONFIGURE="./scripts/do_configure_build.sh"
 "$CONFIGURE" "$TEST32_BUILD_DIR" -DTEST_BUILD_ARCH:string="i386"
 "$CONFIGURE" "$TEST64_BUILD_DIR" -DTEST_BUILD_ARCH:string="x86_64"
 
-cmake --build "$TEST32_BUILD_DIR" -- -j8
-cmake --build "$TEST64_BUILD_DIR" -- -j8
+BUILD="./scripts/do_build.sh"
+"$BUILD" "$TEST32_BUILD_DIR"
+"$BUILD" "$TEST64_BUILD_DIR"
 
 pushd "$TEST32_BUILD_DIR"
 echo "Running tests for the i386 kernel."
