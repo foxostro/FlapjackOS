2387bcaeb44ed76a663992e6b34c7da34dc64276
foxostro <foxostro@gmail.com>
2019-01-25 00:07:32 -0800

    Remove output filtering from test.sh too.

--- scripts/test.sh
+++ scripts/test.sh
@@ -15,18 +15,10 @@ BUILD="./scripts/do_build.sh"
 "$BUILD" "$TEST64_BUILD_DIR"
 
 #pushd "$TEST32_BUILD_DIR" >/dev/null
-#ctest --output-on-failure \
-#    | sed -E 's/^\.\.\/\.\.\/(.*):/\1:/' \
-#    | GREP_COLOR='01;36' grep --color=always -E '^.*:\d+$|$' \
-#    | GREP_COLOR='01;32' grep --color=always -E '^.*\b0 tests failed.*|$' \
-#    | GREP_COLOR='01;31' grep --color=always -E '^.*\b[1-9]\d* tests failed.*|$'
+#ctest --output-on-failure
 #popd >/dev/null
 #echo
 
 pushd "$TEST64_BUILD_DIR" >/dev/null
-ctest --output-on-failure \
-    | sed -E 's/^\.\.\/\.\.\/(.*):/\1:/' \
-    | GREP_COLOR='01;36' grep --color=always -E '^.*:\d+:\d+:|$' \
-    | GREP_COLOR='01;32' grep --color=always -E '^.*\b0 tests failed.*|$' \
-    | GREP_COLOR='01;31' grep --color=always -E '^.*\b[1-9]\d* tests failed.*|$'
+ctest --output-on-failure
 popd >/dev/null
