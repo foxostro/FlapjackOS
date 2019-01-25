0a96ea84907a95996fd7147b8b17cdc18336dd6f
foxostro <foxostro@gmail.com>
2019-01-25 00:46:37 -0800

    Must include stdbool.h in ctype.h when including from c code.

--- Common/include/flapjack_libc/ctype.h
+++ Common/include/flapjack_libc/ctype.h
@@ -1,6 +1,12 @@
 #ifndef FLAPJACKOS_COMMON_INCLUDE_FLAPJACKLIBC_CTYPE_H
 #define FLAPJACKOS_COMMON_INCLUDE_FLAPJACKLIBC_CTYPE_H
 
+#ifdef __cplusplus
+extern "C" {
+#else
+#include <stdbool.h>
+#endif
+
 static inline bool isprint(int c)
 {
     return (c >= ' ') && (c <= 126);
@@ -10,5 +16,9 @@ static inline int isdigit(int c)
 {
     return (c >= '0') && (c <= '9');
 }
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
 
 #endif // FLAPJACKOS_COMMON_INCLUDE_FLAPJACKLIBC_CTYPE_H
