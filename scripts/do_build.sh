2f1c779e6b6f22fe046179a3d73f545447cab6ae
foxostro <foxostro@gmail.com>
2019-01-25 00:06:26 -0800

    I've decided I don't like the output filtering in do_build.sh

--- scripts/do_build.sh
+++ scripts/do_build.sh
@@ -2,9 +2,4 @@
 set -e
 set -o pipefail
 BUILD_DIR="$1"
-cmake --build "$BUILD_DIR" \
-    | sed -E 's/^\.\.\/\.\.\/(.*):/\1:/' \
-    | GREP_COLOR='01;36' grep --color=always -E '^.*:\d+:\d+:|$' \
-    | GREP_COLOR='01;31' grep --color=always -E 'error:|$' \
-    | GREP_COLOR='01;32' grep --color=always -E '\w*~*\^~*|$' \
-  || exit 1
+cmake --build "$BUILD_DIR" || exit 1
