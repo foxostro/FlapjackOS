fccdbeddf2d31ebafc77a6548aa945c2d0c269f8
foxostro <foxostro@gmail.com>
2019-01-25 23:49:52 -0800

    Add stubs for remaining functions in the C++ ABI.

--- Common/src/c_plus_plus_support.cpp
+++ Common/src/c_plus_plus_support.cpp
@@ -8,39 +8,37 @@
 #include "flapjack_libc/assert.h"
 #include <common/logger.hpp>
 
-namespace __cxxabiv1 {
-    struct __class_type_info {
-        virtual ~__class_type_info() = default;
-    } my__class_type_info;
-
-    struct __si_class_type_info {
-        virtual ~__si_class_type_info() = default;
-    } my__si_class_type_info;
-}
-
 extern "C"
-void* __cxa_allocate_exception(size_t thrown_size)
+void* __cxa_allocate_exception(size_t thrown_size) throw()
 {
+    TRACE("stub");
     TRACE("thrown_size --> %u", (unsigned)thrown_size);
+    panic("__cxa_allocate_exception");
     return nullptr;
 }
 
 extern "C"
-void __cxa_free_exception(void *thrown_exception)
+void __cxa_free_exception(void *thrown_exception) throw()
 {
+    TRACE("stub");
     TRACE("thrown_exception --> %p", thrown_exception);
+    panic("__cxa_free_exception");
 }
 
 extern "C"
-void __cxa_begin_catch()
+void* __cxa_allocate_dependent_exception() throw()
 {
-    TRACE("__cxa_begin_catch");
+    TRACE("stub");
+    panic("__cxa_allocate_dependent_exception");
+    return nullptr;
 }
 
 extern "C"
-void __cxa_end_catch()
+void __cxa_free_dependent_exception(void* dependent_exception) throw()
 {
-    TRACE("__cxa_end_catch");
+    TRACE("stub");
+    TRACE("dependent_exception --> %p", dependent_exception);
+    panic("__cxa_free_dependent_exception");
 }
 
 using UnexpectedHandler = void(*)(void);
@@ -65,11 +63,11 @@ struct __cxa_exception {
 
 extern "C"
 void __cxa_throw(void* thrown_exception,
-                 struct type_info* info,
+                 struct type_info* tinfo,
                  void (*dest)(void*))
 {
-    TRACE("begin");
-    (void)info;
+    TRACE("stub");
+    (void)tinfo;
     (void)dest;
 
     __cxa_exception *header = ((__cxa_exception*)thrown_exception - 1);
@@ -77,6 +75,102 @@ void __cxa_throw(void* thrown_exception,
 
     TRACE("exception was not handled in __cxa_throw! We must abort now.");
     abort();
+}
+
+extern "C"
+void* __cxa_get_exception_ptr(void* exception_object) throw()
+{
+    TRACE("stub");
+    TRACE("exception_object --> %p", exception_object);
+    panic("__cxa_get_exception_ptr");
+    return nullptr;
+}
+
+extern "C"
+void* __cxa_begin_catch(void* exception_object) throw()
+{
+    TRACE("stub");
+    TRACE("exception_object --> %p", exception_object);
+    panic("__cxa_begin_catch");
+    return nullptr;
+}
+
+extern "C"
+void __cxa_end_catch()
+{
+    TRACE("stub");
+    panic("__cxa_end_catch");
+}
+
+extern "C"
+std::type_info* __cxa_current_exception_type()
+{
+    TRACE("stub");
+    panic("__cxa_current_exception_type");
+    return nullptr;
+}
+
+extern "C"
+void __cxa_rethrow()
+{
+    TRACE("stub");
+    panic("__cxa_rethrow");
+}
+
+extern "C"
+void* __cxa_current_primary_exception() throw()
+{
+    TRACE("stub");
+    panic("__cxa_current_primary_exception");
+}
+
+extern "C"
+void __cxa_decrement_exception_refcount(void* primary_exception) throw()
+{
+    TRACE("stub");
+    TRACE("primary_exception --> %p", primary_exception);
+    panic("__cxa_decrement_exception_refcount");
+}
+
+struct __cxa_eh_globals;
+extern "C"
+__cxa_eh_globals* __cxa_get_globals() throw()
+{
+    TRACE("stub");
+    panic("__cxa_get_globals");
+    return nullptr;
+}
+
+extern "C"
+__cxa_eh_globals* __cxa_get_globals_fast() throw()
+{
+    TRACE("stub");
+    panic("__cxa_get_globals_fast");
+    return nullptr;
+}
+
+extern "C"
+void __cxa_increment_exception_refcount(void* primary_exception) throw()
+{
+    TRACE("stub");
+    TRACE("primary_exception --> %p", primary_exception);
+    panic("__cxa_increment_exception_refcount");
+}
+
+extern "C"
+void __cxa_rethrow_primary_exception(void* primary_exception)
+{
+    TRACE("stub");
+    TRACE("primary_exception --> %p", primary_exception);
+    panic("__cxa_rethrow_primary_exception");
+}
+
+extern "C"
+bool __cxa_uncaught_exception() throw()
+{
+    TRACE("stub");
+    panic("__cxa_uncaught_exception");
+    return false;
 }
 
 extern "C"
@@ -97,16 +191,279 @@ __gxx_personality_v0(int version,
 }
 
 extern "C"
+int __cxa_guard_acquire(uint64_t* guard_object)
+{
+    TRACE("stub");
+    TRACE("guard_object --> %p", guard_object);
+    panic("__cxa_guard_acquire");
+    return 0;
+}
+
+extern "C"
+void __cxa_guard_release(uint64_t* guard_object)
+{
+    TRACE("stub");
+    TRACE("guard_object --> %p", guard_object);
+    panic("__cxa_guard_release");
+}
+
+extern "C"
+void __cxa_guard_abort(uint64_t* guard_object)
+{
+    TRACE("stub");
+    TRACE("guard_object --> %p", guard_object);
+    panic("__cxa_guard_abort");
+}
+
+extern "C"
+void* __cxa_vec_new(size_t element_count,
+                    size_t element_size,
+                    size_t padding_size,
+                    void (*constructor)(void*),
+                    void (*destructor)(void*))
+{
+    (void)element_count;
+    (void)element_size;
+    (void)padding_size;
+    (void)constructor;
+    (void)destructor;
+    TRACE("stub");
+    panic("__cxa_vec_new");
+    return nullptr;
+}
+
+extern "C"
+void* __cxa_vec_new2(size_t element_count,
+                     size_t element_size,
+                     size_t padding_size,
+                     void (*constructor)(void*),
+                     void (*destructor)(void*),
+                     void* (*alloc)(size_t),
+                     void (*dealloc)(void*))
+{
+    (void)element_count;
+    (void)element_size;
+    (void)padding_size;
+    (void)constructor;
+    (void)destructor;
+    (void)alloc;
+    (void)dealloc;
+    TRACE("stub");
+    panic("__cxa_vec_new2");
+    return nullptr;
+}
+
+extern "C"
+void* __cxa_vec_new3(size_t element_count,
+                     size_t element_size,
+                     size_t padding_size,
+                     void (*constructor)(void*),
+                     void (*destructor)(void*),
+                     void* (*alloc)(size_t),
+                     void (*dealloc)(void*, size_t))
+{
+    (void)element_count;
+    (void)element_size;
+    (void)padding_size;
+    (void)constructor;
+    (void)destructor;
+    (void)alloc;
+    (void)dealloc;
+    TRACE("stub");
+    panic("__cxa_vec_new3");
+    return nullptr;
+}
+
+extern "C"
+void __cxa_vec_ctor(void* array_address,
+                    size_t element_count,
+                    size_t element_size,
+                    void (*constructor)(void*),
+                    void (*destructor)(void*))
+{
+    (void)array_address;
+    (void)element_count;
+    (void)element_size;
+    (void)constructor;
+    (void)destructor;
+    TRACE("stub");
+    panic("__cxa_vec_ctor");
+}
+
+extern "C"
+void __cxa_vec_dtor(void* array_address,
+                    size_t element_count,
+                    size_t element_size,
+                    void (*destructor)(void*))
+{
+    (void)array_address;
+    (void)element_count;
+    (void)element_size;
+    (void)destructor;
+    TRACE("stub");
+    panic("__cxa_vec_dtor");
+}
+
+extern "C"
+void __cxa_vec_cleanup(void* array_address,
+                       size_t element_count,
+                       size_t element_size,
+                       void (*destructor)(void*))
+{
+    (void)array_address;
+    (void)element_count;
+    (void)element_size;
+    (void)destructor;
+    TRACE("stub");
+    panic("__cxa_vec_cleanup");
+}
+
+extern "C"
+void __cxa_vec_delete(void* array_address,
+                      size_t element_size,
+                      size_t padding_size,
+                      void (*destructor)(void*))
+{
+    (void)array_address;
+    (void)element_size;
+    (void)padding_size;
+    (void)destructor;
+    TRACE("stub");
+    panic("__cxa_vec_delete");
+}
+
+extern "C"
+void __cxa_vec_delete2(void* array_address,
+                       size_t element_size,
+                       size_t padding_size,
+                       void (*destructor)(void*),
+                       void (*dealloc)(void*))
+{
+    (void)array_address;
+    (void)element_size;
+    (void)padding_size;
+    (void)destructor;
+    (void)dealloc;
+    TRACE("stub");
+    panic("__cxa_vec_delete2");
+}
+
+extern "C"
+void __cxa_vec_delete3(void* array_address,
+                       size_t element_size,
+                       size_t padding_size,
+                       void (*destructor)(void*),
+                       void (*dealloc)(void*, size_t))
+{
+    (void)array_address;
+    (void)element_size;
+    (void)padding_size;
+    (void)destructor;
+    (void)dealloc;
+    TRACE("stub");
+    panic("__cxa_vec_delete3");
+}
+
+extern "C"
+void __cxa_vec_cctor(void* dest_array,
+                     void* src_array,
+                     size_t element_count,
+                     size_t element_size,
+                     void (*constructor)(void*, void*),
+                     void (*destructor)(void*))
+{
+    (void)dest_array;
+    (void)src_array;
+    (void)element_count;
+    (void)element_size;
+    (void)constructor;
+    (void)destructor;
+    TRACE("stub");
+    panic("__cxa_vec_cctor");
+}
+
+extern "C"
+void (*__cxa_new_handler)();
+
+extern "C"
+void (*__cxa_terminate_handler)();
+
+extern "C"
+void (*__cxa_unexpected_handler)();
+
+extern "C" __attribute__((noreturn))
+void __cxa_bad_cast()
+{
+    TRACE("stub");
+    panic("__cxa_bad_cast");
+}
+
+extern "C" __attribute__((noreturn))
+void __cxa_bad_typeid()
+{
+    TRACE("stub");
+    panic("__cxa_bad_typeid");
+}
+
+extern "C" __attribute__((noreturn))
+void __cxa_pure_virtual(void)
+{
+    TRACE("stub");
+    panic("__cxa_pure_virtual");
+}
+
+extern "C" __attribute__((noreturn))
 void __cxa_throw_bad_array_new_length(void)
 {
+    TRACE("stub");
     panic("__cxa_throw_bad_array_new_length");
 }
 
-// The C++ runtime needs this function in case a pure virtual function call is
-// made. Though, that should not be possible, anyhow.
-extern "C" void __cxa_pure_virtual(void)
+extern "C" __attribute__((noreturn))
+void __cxa_call_unexpected(void*)
 {
-    panic("__cxa_pure_virtual");
+    TRACE("stub");
+    panic("__cxa_call_unexpected");
+}
+
+extern "C"
+char* __cxa_demangle(const char* mangled_name,
+                     char* output_buffer,
+                     size_t* length,
+                     int* status)
+{
+    (void)mangled_name;
+    (void)output_buffer;
+    (void)length;
+    (void)status;
+    TRACE("stub");
+    panic("__cxa_demangle");
+    return nullptr;
+}
+
+namespace __cxxabiv1 {
+    struct __class_type_info {
+        virtual ~__class_type_info() = default;
+    } my__class_type_info;
+
+    struct __si_class_type_info {
+        virtual ~__si_class_type_info() = default;
+    } my__si_class_type_info;
+}
+
+extern "C"
+void* __dynamic_cast(const void* __src_ptr,
+                     const __cxxabiv1::__class_type_info* __src_type,
+                     const __cxxabiv1::__class_type_info* __dst_type,
+                     ptrdiff_t __src2dst)
+{
+    (void)__src_ptr;
+    (void)__src_type;
+    (void)__dst_type;
+    (void)__src2dst;
+    TRACE("stub");
+    panic("__dynamic_cast");
+    return nullptr;
 }
 
 void* operator new(size_t size)
